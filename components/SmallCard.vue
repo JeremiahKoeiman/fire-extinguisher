e92f565817e1a544c174012d3c33094ec766639d
Jeremiah Koeiman <9001603@student.zadkine.nl>
2021-02-02 14:41:37 +0100

    small cards created

---
+++ components/SmallCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card-container">
+        <img :src="require(`@/assets/images/${smallCard.image || 'fe7.jpg'}`)" alt="Fire Extinguisher" class="image">
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['smallCard']
+    }
+</script>
+
+<style scoped>
+    .card-container {
+        width: 24%;
+        height: 12.5rem;
+        border-radius: 0.5rem;
+        overflow: hidden;
+        margin-bottom: 1.5rem;
+        cursor: pointer;
+    }
+    .image {
+        width: 100%;
+        height: 100%
+    }
+</style>
